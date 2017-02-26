ebb15e2fe0a341be9a2962b11198ed1b29a8ec12
arianamoreno <aremoren@ucsc.edu>
2017-02-25 22:49:02 -0800

    Update main.cpp
    user input stuff

--- main.cpp
+++ main.cpp
@@ -11,5 +11,51 @@ int main(){
 	M.dump();
 	//query(M.Fact_map,"Father");
 	//query(M.Fact_map,"Mother");
+	/* string str;
+string command;
+string rest;
+    
+	std::cout << "Type a command to start" << endl;
 	
+    getline(std::cin, str); //get input and store in str
+    stringstream iss(str);//create ss object
+	getline(iss, command, ' ');//parse first letter for command
+	std::string k = getline(iss, rest);//parses rest of string and stores in rest
+	ofstream fout;
+	fout.open("write.txt");
+	string s = "write.txt"
+	if(command == "r"){	
+		std::cout << "Rule" << k << endl;
+			fout << k;
+			
+			Manipulator.load(s);
+		}
+		else if(command == "f"){
+			std::cout << "Fact" << k << endl;
+			fout << k;
+			
+			Manipulator.load(s);
+		}		
+		else if(command == "l"){
+			std::cout << "load";
+			Manipulator.load();}
+	/*} else if(command == "d"){
+			std::cout << "drop";
+			if()
+				Fact_Map.find(name)->
+				else if ()
+					Rule_map.find(name)->
+					
+			/* determine f or r. find data in vector and delete
+			//Fact_map.find(name)->second->vstring.push_back(data);
+		}
+		
+		else if(command == "i"){
+			std::cout << "issuing query";}
+		else if(command == "dump"){
+				Manipulator.dump();
+				std::cout << "dumped"; 
+				//wrtieFile << std::flush ;} //print all changes to sri file}
+		}else{ std::cout << "command not found";}
+	return 0; */
 }
